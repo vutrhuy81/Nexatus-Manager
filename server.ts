44b37b9e9fb73062c2e0b1388b8dc1376057c8a1
Tran Huy Vu <vutrhuy81@gmail.com>
2026-03-31 09:12:16 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -51,7 +51,7 @@ async function writeLog(user: string, action: string, details: string) {
 // ==========================================
 async function startServer() {
   const app = express();
-  const PORT = process.env.PORT || 3000;
+  const PORT = Number(process.env.PORT) || 3000; //const PORT = process.env.PORT || 3000;
 
   app.use(cors());
   app.use(express.json());
